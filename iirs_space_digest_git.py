8a5e80bbd0cf613fbfdb10f95e828e843043b3a5
iirslibrary <iirslibrary@gmail.com>
2026-06-12 12:49:53 +0530

    Update iirs_space_digest_git.py

--- iirs_space_digest_git.py
+++ iirs_space_digest_git.py
@@ -1257,10 +1257,7 @@ html_body = f"""<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="Expires" content="0">
-<meta name="build-version" content="{build_version}">
+
 <style>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
