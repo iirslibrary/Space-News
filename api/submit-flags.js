b8527183ff8cf9330e0b23453eb9fce034a13731
iirslibrary <iirslibrary@gmail.com>
2026-06-16 15:34:45 +0530

    Update submit-flags.js
    updated session init

--- api/submit-flags.js
+++ api/submit-flags.js
@@ -292,6 +292,19 @@ export default async function handler(req, res) {
       }).format(new Date());
     }
 
+    if (action === 'session_init') {
+      if (newSessionCookie) {
+        res.setHeader('Set-Cookie', [newSessionCookie]);
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: 'Session created successfully',
+        reviewer: reviewerEmail,
+        user: sessionUser
+      });
+    }
+
     if (action === 'publish') {
       const istDate = getIstDate();
 
